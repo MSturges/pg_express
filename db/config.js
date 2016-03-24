f41bee025d4ec6bb960326586fd43ccfd371e57d
MSturges <Sturges3232@gmail.com>
2016-03-24 15:20:05 -0600

    added db schema, add pgg dem, initial tea app

---
+++ db/config.js
@@ -0,0 +1,1 @@
+exports.conString = 'postgress://localhost/pgteas';
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var path = require('path');
+var conString = require(path.join(__dirname, '..', 'db/config')).conString;
+
+console.log(conString);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/teas', function(req, res, next) {
+
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      done();
+      return {
+        success: false,
+        data: err
+      };
+    }
+
+    var query, results = { result_count: 0, data: [] };
+    query = client.query("SELECT * FROM teas");
+
+    query.on('row', function(row) {
+      results.data.push(row);
+    });
+
+    query.on('error', function() {
+      // handle query error
+    });
+
+    query.on('end', function(result) {
+      done();
+      results.result_count = result.rowCount;
+      res.json(results);
+    });
+  });
+
 });
 
 module.exports = router;
